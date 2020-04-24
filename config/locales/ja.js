bc7a5fd8407bf4354eaec4f97bf39ac6fcb6e2b1
aoisensi <aoisensi@users.noreply.github.com>
2020-04-24 22:02:21 +0900

    Create ja.js

---
+++ config/locales/ja.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tHome: 'ホーム',
+  tRelativePosts: '相対投稿',
+  tFollowTwitterDescription: '私のTwitterをフォローしてみては?',
+  tTags: 'タグ',
+  tIndTitle: '全投稿',
+  taIndKeywords: [`ブログ`, `gatsby`, `javascript`, `react`],
+  tfIndCountPosts: count => `投稿数: ${count}`,
+  tfTagHeader: (totalCount, tag) =>
+    `"${tag}"がついた投稿数: ${totalCount}`,
+  t404Title: '見つかりません',
+  t404Content: 'あなたはなにもないところに来てしまいました… 悲しいですね',
+};
